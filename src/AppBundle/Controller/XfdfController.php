e3661e73f2a9bb56c6f7223b232624661f9a33f6
Sherbieny <sherbieny@outlook.com>
2017-07-31 19:12:38 +0300

    xfdf rest working

---
+++ src/AppBundle/Controller/XfdfController.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sherbieny
+ * Date: 7/26/17
+ * Time: 3:04 PM
+ */
+
+namespace AppBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
+use FOS\RestBundle\View\ViewHandlerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class XfdfController extends FOSRestController
+{
+    /**
+     * @Route("/convert", name="default_convert")
+     *
+     */
+    public function populateFormAction(){
+
+
+
+        $pdfForm = "/home/sherbieny/Documents/Work/Tecla/MCPDF/sample.pdf";
+
+        $xfdfFile = "/home/sherbieny/Documents/Work/Tecla/MCPDF/sample.xfdf";
+
+        $jarfile = "/home/sherbieny/Documents/Work/Tecla/MCPDF/mcpdf.jar";
+
+        $result = "/home/sherbieny/Documents/Work/Tecla/MCPDF/output.pdf";
+
+        $command = "java -jar $jarfile $pdfForm fill_form - output - < $xfdfFile > $result";
+
+
+        $out = exec($command, $output);
+        //dump($output);
+        dump($out);
+
+
+
+
+
+
+        return $this->render('default/convert.html.twig');
+
+
+    }
+
+   /**
+     * @param $form
+     * @param $data
+     * @return string
+     * @Rest\Get("/convert/{form}/{data}")
+     */
+    public function getDataAction($form, $data){
+
+        $jarfile = "/home/sherbieny/Documents/Work/Tecla/MCPDF/mcpdf.jar";
+
+        $result = "/home/sherbieny/Documents/Work/Tecla/MCPDF/output.pdf";
+
+        $command = "java -jar $jarfile $form fill_form - output - < $data > $result";
+
+        $result = exec($command, $output);
+
+
+        return $result;
+    }
+
+}
