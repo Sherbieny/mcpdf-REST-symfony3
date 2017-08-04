b2b4896ad4cbc16e0ff8b41d3440ccdcf62b596d
Sherbieny <sherbieny@outlook.com>
2017-08-04 15:11:25 +0200

    Part 2 updated

--- src/AppBundle/Controller/MpdfController.php
+++ src/AppBundle/Controller/MpdfController.php
@@ -11,6 +11,8 @@ namespace AppBundle\Controller;
 
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class MpdfController extends FOSRestController
@@ -22,31 +24,25 @@ class MpdfController extends FOSRestController
      */
     public function createPDFAction(){
         $mpdfService = $this->get('tfox.mpdfport');
-        $html = "zz";
-        $html2 = "<h1> Hello </h1>";
-        $mpdf = $mpdfService->getMpdf(['tempDir' => __DIR__.'/tmp']);
+        $html = "<h1> Hello </h1>";
 //        $mpdf->allow_charset_conversion=true;
 //        $mpdf->charset_in = 'iso-8859-4';
-
-        $mpdf->WriteHTML($html2);
+        $mpdf = $mpdfService->getMpdf();
+        $mpdf->WriteHTML($html);
         $mpdf->Output();
         exit;
 
     }
 
     /**
-     * @return \Symfony\Component\HttpFoundation\Response
+     * @param $htmlSource
      * @Rest\Get("/create/{htmlSource}")
      */
-    public function generatePDFAction($htmlSource){
+    public function createPDFFromSourceAction($htmlSource){
         $mpdfService = $this->get('tfox.mpdfport');
-        $html = "zz";
-        $html2 = file_get_contents($htmlSource);
+        $html = file_get_contents($htmlSource);
         $mpdf = $mpdfService->getMpdf();
-//        $mpdf->allow_charset_conversion=true;
-//        $mpdf->charset_in = 'iso-8859-4';
-        $mpdf->debug = true;
-        $mpdf->WriteHTML($html2);
+        $mpdf->WriteHTML($html);
         $mpdf->Output();
         exit;
     }
